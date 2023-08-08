c715c51a0e701cf2268705a36e47ed7df30a3454
Yevhen Laichenkov <elaichenkov@gmail.com>
2023-08-08 13:26:38 +0300

    add(library): custom matchers and types

---
+++ index.d.ts
@@ -0,0 +1,412 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainer<Subject> {
+    /**
+     * Custom Chai assertion that checks that the response status message is equal to given message
+     * @example
+     * ```
+     * cy.request('/users').should('have.statusMessage', 'OK');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.have.statusMessage('OK');
+     * });
+     * ```
+     * */
+    (chainer: 'have.statusMessage', message: string): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to given status code
+     *
+     * @example
+     ```
+    cy.request('/users').should('have.status', 200);
+    or
+    cy.request('/users').should((response) => {
+       expect(response).to.have.status(200);
+    });
+    ```
+    * */
+    (chainer: 'have.status'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 200
+     * @example
+     * ```
+     * cy.request('/users').should('be.successful');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.successful;
+     * });
+     * ```
+     * */
+    (chainer: 'be.successful'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 201
+     * @example
+     * ```
+     * cy.request('/users').should('be.created');
+     * or
+     * cy.request('/users').should((response) => {
+     *   expect(response).to.be.created;
+     * });
+     * ```
+     * */
+    (chainer: 'be.created'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code to be OK
+     * @example
+     * ```
+     * cy.request('/users').should('be.ok');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.ok;
+     * });
+     * ```
+     * */
+    (chainer: 'be.ok'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 204
+     * @example
+     * ```
+     * cy.request('/users').should('be.noContent');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.noContent;
+     * });
+     * ```
+     * */
+    (chainer: 'be.noContent'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 400
+     * @example
+     * ```
+     * cy.request('/users').should('be.badRequest');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.badRequest;
+     * });
+     * ```
+     * */
+    (chainer: 'be.badRequest'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 401
+     * @example
+     * ```
+     * cy.request('/users').should('be.unauthorized');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.unauthorized;
+     * });
+     * ```
+     * */
+    (chainer: 'be.unauthorized'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 403
+     * @example
+     * ```
+     * cy.request('/users').should('be.forbidden');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.forbidden;
+     * });
+     * ```
+     * */
+    (chainer: 'be.forbidden'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 404
+     * @example
+     * ```
+     * cy.request('/users').should('be.notFound');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.notFound;
+     * });
+     * ```
+     * */
+    (chainer: 'be.notFound'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code to be client error
+     * @example
+     * ```
+     * cy.request('/users').should('be.clientError');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.clientError;
+     * });
+     * ```
+     * */
+    (chainer: 'be.clientError'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 500
+     * @example
+     * ```
+     * cy.request('/users').should('be.serverError');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.serverError;
+     * });
+     * ```
+     * */
+    (chainer: 'be.serverError'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 501
+     * @example
+     * ```
+     * cy.request('/users').should('be.notImplemented');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.notImplemented;
+     * });
+     * ```
+     * */
+    (chainer: 'be.notImplemented'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 502
+     * @example
+     * ```
+     * cy.request('/users').should('be.badGateway');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.badGateway;
+     * });
+     * ```
+     * */
+    (chainer: 'be.badGateway'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code is equal to 503
+     * @example
+     * ```
+     * cy.request('/users').should('be.serviceUnavailable');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.serviceUnavailable;
+     * });
+     * ```
+     * */
+    (chainer: 'be.serviceUnavailable'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code to be server error
+     * @example
+     * ```
+     * cy.request('/users').should('be.serverError');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.serverError;
+     * });
+     * ```
+     * */
+    (chainer: 'be.serverError'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response status code to be OK
+     * @example
+     * ```
+     * cy.request('/users').should('be.ok');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.ok;
+     * });
+     * ```
+     * */
+    (chainer: 'be.ok'): Chainable<Subject>;
+
+    /**
+     *  Custom Chai assertion that checks that the response body is equal to JSON body
+     * @example
+     * ```
+     * cy.request('/users').should('have.body', { name: 'Jane' });
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.have.body({ name: 'Jane' });
+     * });
+     * ```
+     * */
+    (chainer: 'have.body', json: any): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body is equal to JSON body
+     * @example
+     * ```
+     * cy.request('/users').should('haveBody', { name: 'Jane' });
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.haveBody({ name: 'Jane' });
+     * });
+     * ```
+     * */
+    (chainer: 'haveBody', json: any): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body is equal to JSON body
+     * @example
+     * ```
+     * cy.request('/users').should('haveJson', { name: 'Jane' });
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.haveJson({ name: 'Jane' });
+     * });
+     * ```
+     * */
+    (chainer: 'haveJson', json: any): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body contains JSON body
+     * @example
+     * ```
+     * cy.request('/users').should('containBody', { name: 'Jane' });
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.containBody({ name: 'Jane' });
+     * });
+     * ```
+     * */
+    (chainer: 'containBody', json: any): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body contains JSON body
+     * @example
+     * ```
+     * cy.request('/users').should('containJson', { name: 'Jane' });
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.containJson({ name: 'Jane' });
+     * });
+     * ```
+     * */
+    (chainer: 'containJson', json: any): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body is equal to text
+     * @example
+     * ```
+     * cy.request('/users').should('haveText', 'Jane');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.haveText('Jane');
+     * });
+     * ```
+     * */
+    (chainer: 'haveText', text: string): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response body contains text
+     * @example
+     * ```
+     * cy.request('/users').should('containText', 'Jane');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.containText('Jane');
+     * });
+     * ```
+     * */
+    (chainer: 'containText', text: string): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response header is equal to given header
+     * @example
+     * ```
+     * cy.request('/users').should('have.header', 'content-type', 'application/json');
+     * cy.request('/users').should('have.header', 'content-type');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.have.header('content-type', 'application/json');
+     * });
+     * ```
+     * */
+    (chainer: 'have.header', header: string, value?: string): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response content type is equal to given content type
+     * @example
+     * ```
+     * cy.request('/users').should('have.contentType', 'application/json');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.have.contentType('application/json');
+     * });
+     * ```
+     * */
+    (chainer: 'have.contentType', contentType: string): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response content type is equal to JSON content type
+     * @example
+     * ```
+     * cy.request('/users').should('be.json');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.json;
+     * });
+     * ```
+     * */
+    (chainer: 'be.json'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response content type is equal to HTML content type
+     * @example
+     * ```
+     * cy.request('/users').should('be.html');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.html;
+     * });
+     * ```
+     * */
+    (chainer: 'be.html'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response content type is equal to XML content type
+     * @example
+     * ```
+     * cy.request('/users').should('be.xml');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.xml;
+     * });
+     * ```
+     * */
+    (chainer: 'be.xml'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response content type is equal to text content type
+     * @example
+     * ```
+     * cy.request('/users').should('be.text');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.be.text;
+     * });
+     * ```
+     * */
+    (chainer: 'be.text'): Chainable<Subject>;
+
+    /**
+     * Custom Chai assertion that checks that the response cookies contain given cookie
+     * @example
+     * ```
+     * cy.request('/users').should('have.cookie', 'session_id');
+     * cy.request('/users').should('have.cookie', 'session_id', '1234');
+     * or
+     * cy.request('/users').should((response) => {
+     *  expect(response).to.have.cookie('session_id');
+     *  expect(response).to.have.cookie('session_id', '1234');
+     * });
+     * ```
+     * */
+    (chainer: 'have.cookie', cookie: string, value?: string): Chainable<Subject>;
+  }
+}
---
+++ index.js
@@ -0,0 +1,255 @@
+/* eslint-disable no-undef */
+chai.config.truncateThreshold = 300;
+
+chai.use(({ Assertion }) => {
+  const assert = (value) => new chai.Assertion(value);
+
+  const assertions = new Map([
+    [
+      ['header', 'haveHeader'],
+      function toHaveHeader(expectedHeader, expectedValue) {
+        const lowerCaseHeader = expectedHeader.toLowerCase();
+        const actualHeaders = this._obj.headers;
+
+        assert(actualHeaders).to.have.property(expectedHeader);
+
+        if (expectedValue) {
+          assert(actualHeaders[lowerCaseHeader]).to.eq(expectedValue);
+        }
+      },
+    ],
+    [
+      'contentType',
+      function toHaveContentType(expectedContentType) {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq(expectedContentType);
+      },
+    ],
+    [
+      ['status', 'statusCode', 'haveStatusCode'],
+      function toHaveStatusCode(expectedStatusCode) {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(expectedStatusCode);
+      },
+    ],
+    [
+      'statusMessage',
+      function toHaveStatusMessage(expectedStatusCode) {
+        const actualStatusCode = this._obj.statusText;
+        assert(actualStatusCode).to.eq(expectedStatusCode);
+      },
+    ],
+    [
+      'successful',
+      function toBeSuccessful() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(200);
+      },
+    ],
+    [
+      'created',
+      function toBeCreated() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(201);
+      },
+    ],
+    [
+      'noContent',
+      function toBeEmpty() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(204);
+      },
+    ],
+    [
+      'badRequest',
+      function toBeBadRequest() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(400);
+      },
+    ],
+    [
+      'unauthorized',
+      function toBeUnauthorized() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(401);
+      },
+    ],
+    [
+      'forbidden',
+      function toBeForbidden() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(403);
+      },
+    ],
+    [
+      'notFound',
+      function toBeNotFound() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(404);
+      },
+    ],
+
+    [
+      'internalError',
+      function toBeInternalError() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(500);
+      },
+    ],
+    [
+      'notImplemented',
+      function toBeNotImplemented() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(501);
+      },
+    ],
+    [
+      'badGateway',
+      function toBeBadGateway() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(502);
+      },
+    ],
+    [
+      'serviceUnavailable',
+      function toBeServiceUnavailable() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.eq(503);
+      },
+    ],
+    [
+      'ok',
+      function toBeOk() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.be.oneOf([200, 201]);
+      },
+    ],
+    [
+      'redirected',
+      function toBeRedirected() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.be.oneOf([301, 302]);
+      },
+    ],
+    [
+      'clientError',
+      function toBeClientError() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.be.oneOf([400, 401, 403, 404]);
+      },
+    ],
+    [
+      'serverError',
+      function toBeServerError() {
+        const actualStatusCode = this._obj.status;
+        assert(actualStatusCode).to.be.oneOf([500, 501, 502, 503, 504]);
+      },
+    ],
+    [
+      ['body', 'haveBody', 'haveJson'],
+      function haveBody(expectedBody) {
+        const actualBody = this._obj.body;
+        assert(actualBody).to.deep.eq(expectedBody);
+      },
+    ],
+    [
+      'haveText',
+      function haveText(expectedText) {
+        const actualText = this._obj.body;
+        assert(actualText).to.eq(expectedText);
+      },
+    ],
+    [
+      'containText',
+      function containText(expectedText) {
+        const actualText = this._obj.body;
+        assert(actualText).to.include(expectedText);
+      },
+    ],
+    [
+      ['containBody', 'containJson'],
+      function containBody(expectedBody) {
+        const actualBody = this._obj.body;
+        assert(actualBody).to.deep.include(expectedBody);
+      },
+    ],
+    [
+      ['cookie', 'haveCookie'],
+      function haveCookie(expectedCookieName, expectedCookieValue) {
+        const actualCookies = this._obj.headers['set-cookie'][0];
+        const cookiesArray = actualCookies.split('; ');
+
+        const cookies = {};
+        cookiesArray.forEach((cookie) => {
+          const [name, value] = cookie.split('=');
+          cookies[name] = value;
+        });
+
+        const actualCookieValue = cookies[expectedCookieName];
+
+        assert(cookies).to.have.property(expectedCookieName);
+
+        if (expectedCookieValue) {
+          assert(actualCookieValue).to.eq(expectedCookieValue);
+        }
+      },
+    ],
+    [
+      'json',
+      function toBeJson() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('application/json');
+      },
+    ],
+    [
+      'html',
+      function toBeHtml() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('text/html');
+      },
+    ],
+    [
+      'text',
+      function toBeText() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('text/plain');
+      },
+    ],
+    [
+      'javascript',
+      function toBeJavascript() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('application/javascript');
+      },
+    ],
+    [
+      'css',
+      function toBeCss() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('text/css');
+      },
+    ],
+    [
+      'image',
+      function toBeImage() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('image/png');
+      },
+    ],
+    [
+      'xml',
+      function toBeXml() {
+        const actualContentType = this._obj.headers['content-type'];
+        assert(actualContentType).to.eq('application/xml');
+      },
+    ],
+  ]);
+
+  assertions.forEach((assertionFn, names) => {
+    if (Array.isArray(names)) {
+      names.forEach((name) => Assertion.addMethod(name, assertionFn));
+    } else {
+      Assertion.addMethod(names, assertionFn);
+    }
+  });
+});
